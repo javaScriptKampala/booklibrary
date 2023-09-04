37889823ee3bc032115a6a73de56f00aa34347a4
Bugingo Daniel <danielbugingo1@gmail.com>
2023-09-04 14:36:49 +0300

    Done some code refactoring mainly on the "if" code block

--- server.js
+++ server.js
@@ -5,22 +5,23 @@ var fs = require("fs")
 var server = http.createServer(function (req,res){
     console.log("hi boss", req.url)
 
-    if(req.url === "/me" || "/"){
-        res.writeHead(200,{"content-Type":'text/html'})
+    if(req.url === "/me" || req.url === "/"){
+        res.writeHead(200, {"content-Type":'text/html'});
         fs.createReadStream(__dirname + "/me.html").pipe(res)
-    }
-    if(req.url === "/social" || "/"){
-        res.writeHead(200,{"contnt-Type":'text/html'})
-        fs.createReadStream(__dirname + "/social.html").pipe(res)
-    }
-    if(req.url === "/span" || "/"){
-        res.writeHead(200,{"content-Type": 'text/html'})
-        fs.createReadStream(__dirname + '/span.html').pipe(res)
+    }else if(req.url === "/social" || req.url === "/"){
+        res.writeHead(200, {"contnt-Type":'text/html'});
+        fs.createReadStream(__dirname + "/social.html").pipe(res);
+    }else if(req.url === "/span" || req.url === "/"){
+        res.writeHead(200,{"content-Type": 'text/html'});
+        fs.createReadStream(__dirname + '/span.html').pipe(res);
+    }else {
+        res.writeHead(404, {"content-Type": 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
     }
 
 })
 var PORT = process.env.PORT || 8080
 server.listen(PORT, function(){
-    return console.log(`listening on PORT ${PORT}`)
-})
-console.log("Creative developer")
+    return console.log(`Listening on PORT ${PORT}`);
+});
+console.log("Creative developer");
